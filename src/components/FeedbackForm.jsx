a776bc14821b02ee3edc3084b64dbe42190c0c71
Lucky <nhlalucky9@gmail.com>
2022-04-27 06:38:04 +0200

    refactor(form): added input validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import { useState } from "react";
 import Button from "./shared/Button";
 import Card from "./shared/Card";
 
 function FeedbackForm() {
+  const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState(null);
+
   const onChangeTextHnadler = (event) => {
-    console.log(event.target.value);
+    setText(event.target.value);
+    validateInput()
   };
 
+  const validateInput = () => {
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text && text.length <= 10) {
+      setBtnDisabled(true);
+      setMessage('Text must be at least 11 charactors and above');
+    } else {
+      setBtnDisabled(false);
+      setMessage(null);
+    }
+  }
+
   const onFormSendHandler = (event) => {
-    alert(event);
     console.log(event);
   };
 
@@ -17,15 +34,16 @@ function FeedbackForm() {
       <form>
         <h2>How would you rate your service with us?</h2>
         <div className="input-group">
-          <input
+          <input value={text}
             onChange={onChangeTextHnadler}
             type="text"
             placeholder="Write a review"
           />
-          <Button onClickHandler={onFormSendHandler} type="submit">
+          <Button isDisabled={btnDisabled} onClickHandler={onFormSendHandler} type="submit">
             Send
           </Button>
         </div>
+        {message && <div>{message}</div>}
       </form>
     </Card>
   );
